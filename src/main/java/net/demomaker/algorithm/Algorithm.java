3933e04f8297c5ba71432cef05d064f6480269ef
Demomaker <mike01bedard@gmail.com>
2026-02-05 22:39:41 -0500

    Made fluids be named when counted

--- src/main/java/net/demomaker/algorithm/Algorithm.java
+++ src/main/java/net/demomaker/algorithm/Algorithm.java
@@ -10,7 +10,9 @@ import com.hypixel.hytale.server.core.universe.world.World;
 import net.demomaker.debug.Debugger;
 import net.demomaker.shared.BuildInfo;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Algorithm {
@@ -45,14 +47,21 @@ public class Algorithm {
 
                     BlockType blockType = this.world.getBlockType(currentPos);
 
-                    String blockTypeId = (blockType == null || blockType.getId() == null) ? "Empty" : blockType.getId();
+                    String blockTypeId = "Empty";
 
-                    Box box = getBoundingBox(blockType);
+                    if(blockType != null && blockType.getId() != null) {
+                        blockTypeId = blockType.getId();
+                        if(blockTypeId.equals("Empty")) {
+                            blockTypeId = getFluidTypeName(x, y, z);
+                        }
 
-                    if(!box.isUnitBox()) {
-                        addMultiblockToSet(new Vector3i(currentPos), box, occupiedSpaces);
-                    } else {
-                        occupiedSpaces.add(new Vector3i(currentPos).toString());
+                        Box box = getBoundingBox(blockType);
+
+                        if(!box.isUnitBox()) {
+                            addMultiblockToSet(new Vector3i(currentPos), box, occupiedSpaces);
+                        } else {
+                            occupiedSpaces.add(new Vector3i(currentPos).toString());
+                        }
                     }
 
                     // Update the count map
@@ -64,9 +73,21 @@ public class Algorithm {
         return algorithmOutput;
     }
 
+    private String getFluidTypeName(int x, int y, int z) {
+        int fluidId = this.world.getFluidId(x, y, z);
+        debugger.log(String.valueOf(fluidId));
+        String fluidName = this.getFluidNames().get(fluidId);
+        if(fluidId > 0 && fluidName == null) {
+            fluidName = "Unknown Fluid";
+        }
+        return fluidName;
+    }
+
     private void addMultiblockToSet(Vector3i origin, Box box, Set<String> set) {
         Vector3i min = new Vector3i(origin);
-        Vector3i diffBetweenMinAndMaxBox = new Vector3d(box.getMax()).add(-0.01, -0.01, -0.01).floor().subtract(new Vector3d(box.getMin().floor())).toVector3i();
+        Vector3i diffBetweenMinAndMaxBox = new Vector3d(box.
+
+                getMax()).add(-0.01, -0.01, -0.01).floor().subtract(new Vector3d(box.getMin().floor())).toVector3i();
         Vector3i max = new Vector3i(origin).add(diffBetweenMinAndMaxBox);
 
         debugger.log("min : " + min.toString());
@@ -83,7 +104,7 @@ public class Algorithm {
         }
     }
 
-    public Vector3i generateSmallestVector(Vector3i first_vector, Vector3i second_vector) {
+    private Vector3i generateSmallestVector(Vector3i first_vector, Vector3i second_vector) {
         Vector3i smallestVector = new Vector3i(first_vector);
 
         if(smallestVector.x > second_vector.x) {
@@ -99,7 +120,7 @@ public class Algorithm {
         return smallestVector;
     }
 
-    public Vector3i generateBiggestVector(Vector3i first_vector, Vector3i second_vector) {
+    private Vector3i generateBiggestVector(Vector3i first_vector, Vector3i second_vector) {
         Vector3i biggestVector = new Vector3i(first_vector);
 
         if(biggestVector.x < second_vector.x) {
@@ -115,12 +136,32 @@ public class Algorithm {
         return biggestVector;
     }
 
-    public Box getBoundingBox(BlockType blockType) {
+    private Box getBoundingBox(BlockType blockType) {
         BlockBoundingBoxes blockBoundingBoxes = BlockBoundingBoxes.getAssetMap().getAsset(blockType.getHitboxType());
         try {
             return blockBoundingBoxes.get(0).getBoundingBox();
         } catch (Exception e) {
             return Box.UNIT;
         }
+    }
+
+    private Map<Integer, String> getFluidNames() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(0, "Empty");
+        map.put(1, "Mud");
+        map.put(2, "Red Slime");
+        map.put(3, "Tar");
+        map.put(4, "Poison");
+        map.put(5, "Slime");
+        map.put(6, "Lava");
+        map.put(7, "Water");
+        map.put(8, "Flowing Water");
+        map.put(9, "Flowing Red Slime");
+        map.put(10, "Flowing Tar");
+        map.put(11, "Flowing Lava");
+        map.put(12, "Flowing Mud");
+        map.put(13, "Flowing Poison");
+        map.put(14, "Flowing Slime");
+        return map;
     }
 }
